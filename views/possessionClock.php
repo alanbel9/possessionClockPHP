1c534578a8e8ae746ebcd6d717a528092c38db00
Alán <abellacomaa@gmail.com>
2021-01-18 18:38:49 +0100

    Fix clock styles

--- views/possessionClock.php
+++ views/possessionClock.php
@@ -15,20 +15,9 @@
 <body class="text-center">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<div class="clock-window d-flex w-100 h-100 mx-auto flex-column">
+<script type="text/javascript" src="../assets/js/clock.js"></script>
 
-    <!-- CLOCK JS PROCESS -->
-    <script type="text/javascript" src="../assets/js/clock.js"></script>
-    <main>
-        <section class="text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col clock-container">
-                        <input type="text" id="clock-timer-popup" value="" readonly/>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </main>
-
+<div id="clock-window" class="d-flex w-100 h-100 mx-auto flex-column">
+    <input class="w-100 h-100" type="text" id="clock-timer-popup" value="" readonly/>
+    
 <?php require_once("html/base_bottom.php"); ?>
